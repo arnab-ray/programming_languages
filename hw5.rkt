db2f3dbf6bd9d5ff76de6d89aa2921c9fe43a885
arnab-ray <arnab.ray@udaan.com>
2022-12-10 12:45:17 +0530

    added more solution

--- hw5.rkt
+++ hw5.rkt
@@ -155,9 +155,15 @@
                      (apair (call (var "func") (fst (var "xs")))
                             (call (var "map-xs") (snd (var "xs"))))))))
 
+;; Bind to the Racket variable mupl-mapAddN a mupl function that takes an mupl integer i and returns a mupl
+;; function that takes a mupl list of mupl integers and returns a new mupl list of mupl integers that adds i
+;; to every element of the list. Use mupl-map (a use of mlet is given to you to make this easier).
 (define mupl-mapAddN 
   (mlet "map" mupl-map
-        "CHANGE (notice map is now in MUPL scope)"))
+        (fun "mupl-fun-int" "i"
+	     (fun "mupl-fun-list" "ml-int"
+		  (call (call (var "map") (fun "add-i" "x" (add (var "x") (var "i"))))
+			(var "ml-int"))))))
 
 ;; Challenge Problem
 
@@ -165,12 +171,65 @@
 
 ;; We will test this function directly, so it must do
 ;; as described in the assignment
-(define (compute-free-vars e) "CHANGE")
+(define (compute-free-vars e)
+  (struct res (e fvs)) ; result type of f (could also use a pair)
+    (define (f e) 
+      (cond [(var? e) (res e (set (var-string e)))]
+            [(int? e) (res e (set))]
+            [(add? e) (let ([r1 (f (add-e1 e))]
+                            [r2 (f (add-e2 e))])
+                        (res (add (res-e r1) (res-e r2))
+                             (set-union (res-fvs r1) (res-fvs r2))))]
+            [(ifgreater? e) (let ([r1 (f (ifgreater-e1 e))]
+                                  [r2 (f (ifgreater-e2 e))]
+                                  [r3 (f (ifgreater-e3 e))]
+                                  [r4 (f (ifgreater-e4 e))])
+                              (res (ifgreater (res-e r1) (res-e r2) (res-e r3)    (res-e r4))
+                                  (set-union (res-fvs r1) (res-fvs r2) (res-fvs   r3) (res-fvs r4))))]
+            [(fun? e) (let* ([r (f (fun-body e))]
+                             [fvs (set-remove (res-fvs r) (fun-formal e))]
+                             [fvs (if (fun-nameopt e) 
+                                      (set-remove fvs (fun-nameopt e)) 
+                                      fvs)])
+                        (res (fun-challenge (fun-nameopt e) (fun-formal e) 
+                                            (res-e r) fvs)
+                            fvs))]
+            [(call? e) (let ([r1 (f (call-funexp e))]
+                             [r2 (f (call-actual e))])
+                        (res (call (res-e r1) (res-e r2))
+                             (set-union (res-fvs r1) (res-fvs r2))))]
+            [(mlet? e) (let* ([r1 (f (mlet-e e))]
+                              [r2 (f (mlet-body e))])
+                         (res (mlet (mlet-var e) (res-e r1) (res-e r2))
+                              (set-union (res-fvs r1) (set-remove (res-fvs r2)   (mlet-var e)))))]
+            [(apair? e) (let ([r1 (f (apair-e1 e))]
+                              [r2 (f (apair-e2 e))])
+                          (res (apair (res-e r1) (res-e r2))
+                             (set-union (res-fvs r1) (res-fvs r2))))]
+            [(fst? e) (let ([r (f (fst-e e))])
+                        (res (fst (res-e r))
+                             (res-fvs r)))]
+            [(snd? e) (let ([r (f (snd-e e))])
+                        (res (snd (res-e r))
+                             (res-fvs r)))]
+            [(aunit? e) (res e (set))]
+            [(isaunit? e) (let ([r (f (isaunit-e e))])
+                            (res (isaunit (res-e r))
+                                 (res-fvs r)))]))
+    (res-e (f e)))
 
 ;; Do NOT share code with eval-under-env because that will make
 ;; auto-grading and peer assessment more difficult, so
 ;; copy most of your interpreter here and make minor changes
-(define (eval-under-env-c e env) "CHANGE")
+(define (eval-under-env-c e env)
+  (cond 
+        [(fun-challenge? e)
+         (closure (set-map (fun-challenge-freevars e)
+                           (lambda (s) (cons s (envlookup env s))))
+                  e)]
+         ; call case uses fun-challenge as appropriate
+         ; all other cases the same
+        ...))
 
 ;; Do NOT change this
 (define (eval-exp-c e)
